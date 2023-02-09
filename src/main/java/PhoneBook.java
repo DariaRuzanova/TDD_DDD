67bb42b1c88c6e19180ece9dd98220cd390f8622
d.ruzanova <dashochek-28@mail.ru>
2023-02-09 08:27:10 +0400

    Stub and tests for findByNumber

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -6,11 +6,14 @@ public class PhoneBook {
     public Map<String, String> contactList = new HashMap<>();
 
     public int add(String name, String phoneNumber) {
-        if(!contactList.containsValue(phoneNumber)){
-            contactList.put(name,phoneNumber);
-        }else{
+        if (!contactList.containsValue(phoneNumber)) {
+            contactList.put(name, phoneNumber);
+        } else {
             System.out.println("Контакт с таким номером существует в телефонной книге");
         }
         return contactList.size();
+    }
+    public String findByNumber(String phoneNumber){
+        return null;
     }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,11 +1,17 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PhoneBookTest {
     PhoneBook phoneBook = new PhoneBook();
+    @BeforeAll
+    public static void testsStarted(){
+        System.out.println("Тесты запущены");
+    }
+    @AfterAll
+    public static void testsFinished(){
+        System.out.println("Тесты закончены");
+    }
     @BeforeEach
     public void testStarted(){
         System.out.println("Старт теста");
@@ -14,6 +20,7 @@ class PhoneBookTest {
     public void testFinished(){
         System.out.println("Окончание теста");
     }
+
     @Test
     public void addTest(){
         String name1 = "Petya";
@@ -26,6 +33,15 @@ class PhoneBookTest {
         phoneBook.add(name1,phone1);
         phoneBook.add(name2,phone2);
         int res = phoneBook.add(name3,phone3);
+        assertEquals(expected,res);
+    }
+    @Test
+    public void findByNumber(){
+        String name1 = "Petya";
+        String phone1 = "89171456678";
+        phoneBook.add(name1,phone1);
+        String expected = "Petya";
+        String res = phoneBook.findByNumber(phone1);
         assertEquals(expected,res);
     }
 }
